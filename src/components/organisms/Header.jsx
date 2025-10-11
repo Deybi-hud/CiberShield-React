e3a08897f31724ae7318f39990e97174778e1902
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-10-11 20:49:34 -0300

    DS

---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,23 @@
+import Text from "../atoms/Text";
+import { Button } from "../atoms/Button";
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <aside>
+            <header>
+            <Text className="logo">CiberShield</Text>
+            <li>
+                <Text className="boton-menu" href="/login">
+                <Button className="boton-iniciar-sesion boton-menu">
+                    <i className="bi bi-hand-index-thumb-fill"></i> Iniciar sesión
+                </Button>
+                </Text>
+            </li>
+            </header>
+        </aside>
+    );  
+};
+
+export default Header;
