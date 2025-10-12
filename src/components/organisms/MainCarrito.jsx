453164e66ca7e6d8b7a57b1f5ea4bb078a60cd8b
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-10-12 20:32:21 -0300

    ASD

--- src/components/organisms/MainCarrito.jsx
+++ src/components/organisms/MainCarrito.jsx
@@ -6,32 +6,24 @@ import Footer from "../molecules/Footer";
 
 const MainCarrito = () => {
     return (
-
-        <Wrapper>
-            <Header></Header>
-            <NavCarrito>
-                <main>
-                    <Text variant = "h2" class="titulo-principal">Carrito</Text>
-                    <div class="contenedor-carrito">
-                        <Text variant = "p" id="carrito-vacio" class="carrito-vacio">Tu carrito está vacío. <i class="bi bi-emoji-frown"></i></Text>
-                        <div id="carrito-productos" class="carrito-productos disabled">
-                        </div>
-                        <div id="carrito-acciones" class="carrito-acciones disabled">
-                            <div class="carrito-acciones-derecha">
-                                <div class="carrito-acciones-total">
-                                    <Text variant="p">Total:</Text>
-                                    <Text variant="p" id="total">$0</Text>
-                                </div>
-                                <Button id="carrito-acciones-comprar" class="carrito-acciones-comprar">Comprar ahora</Button>
-                            </div>
-                        </div>
-                        <Text variant="p" id="carrito-comprado" class="carrito-comprado disabled">Muchas gracias por tu compra. <i class="bi bi-emoji-laughing"></i></Text>
+            <main>
+                <Text variant = "h2" class="titulo-principal">Carrito</Text>
+                <div class="contenedor-carrito">
+                    <Text variant = "p" id="carrito-vacio" class="carrito-vacio">Tu carrito está vacío. <i class="bi bi-emoji-frown"></i></Text>
+                    <div id="carrito-productos" class="carrito-productos disabled">
                     </div>
-                </main>
-            </NavCarrito>
-            <Footer>
-            </Footer>
-        </Wrapper>
+                    <div id="carrito-acciones" class="carrito-acciones disabled">
+                        <div class="carrito-acciones-derecha">
+                            <div class="carrito-acciones-total">
+                                <Text variant="p">Total:</Text>
+                                <Text variant="p" id="total">$0</Text>
+                            </div>
+                            <Button id="carrito-acciones-comprar" class="carrito-acciones-comprar">Comprar ahora</Button>
+                        </div>
+                    </div>
+                    <Text variant="p" id="carrito-comprado" class="carrito-comprado disabled">Muchas gracias por tu compra. <i class="bi bi-emoji-laughing"></i></Text>
+                </div>
+            </main>
     );
 };
 
---
+++ src/components/organisms/SidebarCarrito.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../molecules/Header';
+import Footer from '../molecules/Footer';
+import NavCarrito from './NavCarrito';
+
+const SidebarCarrito = () => {
+  return (
+    <aside>
+      <Header />
+      <NavCarrito/>
+      <Footer />
+    </aside>
+  );
+};
+
+export default SidebarCarrito;
