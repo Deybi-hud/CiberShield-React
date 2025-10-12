ec5589847a9fafe238f5dc86e91bd8c236cb659c
Deybi-hud <hu4nt4r@gmail.com>
2025-10-11 21:01:57 -0300

    Update

---
+++ src/components/organisms/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import NavMenu from '../molecules/NavMenu';
+import Footer from './Footer';
+
+const Sidebar = () => {
+    return (
+        <asid>
+            <Header />
+            <NavMenu />
+            <Footer />
+        </asid>
+    );
+};
+export default Sidebar;
