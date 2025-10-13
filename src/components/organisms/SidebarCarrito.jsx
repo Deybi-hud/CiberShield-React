33b7fd688d943ad644c3e5eef4b2a29f73b4b075
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-10-13 20:24:40 -0300

    xd

---
+++ src/components/organisms/SidebarCarrito.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../molecules/Header";
+import Footer from "../molecules/Footer";
+
+
+const SidebarCarrito = () => {
+
+    return (
+        <aside>
+            <Header></Header>
+            <NavCarrito></NavCarrito>
+            <Footer></Footer>
+        </aside>
+
+    );
+}
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,57 +1,18 @@
 import React from "react"; 
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "../styles/pages/Home.css";
+import Wrapper from "../components/Templates/Wrapper";
+import SidebarCarrito from "../components/organisms/SidebarCarrito";
+import NavCarrito from "../components/organisms/NavCarrito";
 
 function Carrito() {
 
     return(
-    <body>
-        <div class="wrapper">
-            <aside>
-                <header>
-                    <h1 class="logo">CiberShield</h1>
-                </header>
-                <nav>
-                    <ul>
-                        <li>
-                            <a class="boton-menu boton-volver" href="./pages/Home.jsx">
-                                <i class="bi bi-arrow-return-left"></i> Seguir comprando
-                            </a>
-                        </li>
-                        <li>
-                            <a class="boton-menu boton-carrito active" href="/html/carrito.html">
-                                <i class="bi bi-cart-fill"></i> Carrito
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <footer>
-                    <p>© 2025 CiberShield</p>
-                </footer>
-            </aside>
+        <Wrapper>
+            <SidebarCarrito></SidebarCarrito>
             
-            <main>
-                <h2 class="titulo-principal">Carrito</h2>
-                <div class="contenedor-carrito">
-                    <p id="carrito-vacio" class="carrito-vacio">Tu carrito está vacío. <i class="bi bi-emoji-frown"></i></p>
-                    <div id="carrito-productos" class="carrito-productos disabled">
-                    </div>
-                    <div id="carrito-acciones" class="carrito-acciones disabled">
-                        <div class="carrito-acciones-derecha">
-                            <div class="carrito-acciones-total">
-                                <p>Total:</p>
-                                <p id="total">$0</p>
-                            </div>
-                            <button id="carrito-acciones-comprar" class="carrito-acciones-comprar">Comprar ahora</button>
-                        </div>
-                    </div>
-                    <p id="carrito-comprado" class="carrito-comprado disabled">Muchas gracias por tu compra. <i class="bi bi-emoji-laughing"></i></p>
-                </div>
-            </main>
-
-        </div>
-    </body>
-    )
-}
+        </Wrapper>
+    );
+};
 
 export default Carrito;
