cf3836156faf45734f829f32b454dcaaaa53356a
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-10-14 18:25:27 -0300

    recu1
    intento recuperacion 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Carrito from './pages/Carrito'; 
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 function App() {
+  const [productosEnCarrito, setProductosEnCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto) => {
+    setProductosEnCarrito((prevCarrito) => {
+      const existente = prevCarrito.find((item) => item.id === producto.id);
+      return existente
+        ? prevCarrito.map((item) =>
+            item.id === producto.id
+              ? { ...item, cantidad: item.cantidad + 1 }
+              : item
+          )
+        : [...prevCarrito, { ...producto, cantidad: 1 }];
+    });
+  };
+
+  const eliminarDelCarrito = (productoId) => {
+    setProductosEnCarrito((prevCarrito) =>
+      prevCarrito
+        .map((item) =>
+          item.id === productoId
+            ? { ...item, cantidad: item.cantidad - 1 }
+            : item
+        )
+        .filter((item) => item.cantidad > 0)
+    );
+  };
+
+  const vaciarCarrito = () => {
+    setProductosEnCarrito([]);
+  };
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/carrito" element={<Carrito />} />
+        <Route 
+          path="/" 
+          element={
+            <Home 
+              productosEnCarrito={productosEnCarrito}
+              agregarAlCarrito={agregarAlCarrito}
+            />
+          } 
+        />
+        <Route 
+          path="/carrito" 
+          element={
+            <Carrito 
+              productosEnCarrito={productosEnCarrito}
+              eliminarDelCarrito={eliminarDelCarrito}
+              vaciarCarrito={vaciarCarrito}
+            />
+          } 
+        />
       </Routes>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { CarritoProvider } from './pages/CarritoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <CarritoProvider>
-        <App />
-      </CarritoProvider>
-    </BrowserRouter>
-  </StrictMode>,
+ <StrictMode>
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+ </StrictMode>,
 )
