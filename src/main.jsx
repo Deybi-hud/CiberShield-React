9e5348309e875ee8bec1346cf01842cd76c119b7
Deybi-hud <hu4nt4r@gmail.com>
2025-10-16 23:26:59 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { CarritoProvider } from './Context/CarritoContext.jsx'
+import { CarritoProvider } from './context/CarritoContext.jsx'
 import './styles/Global.css'
 import './styles/Utils.css'
 
