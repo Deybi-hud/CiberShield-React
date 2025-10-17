5e4349170060525be0e09e953a9ad16cfccde89b
Deybi-hud <hu4nt4r@gmail.com>
2025-10-17 18:26:10 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext.jsx'
-import  './styles/Global.css'
 import './styles/Utils.css'
 
 createRoot(document.getElementById('root')).render(
