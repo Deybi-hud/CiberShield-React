1d45d5bd2d91ec6469ee366368c2008165fc06d3
Deybi-hud <hu4nt4r@gmail.com>
2025-11-04 15:48:44 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext.jsx'
 import './styles/Utils.css'
-import './styles/Global.css'
+
+
 createRoot(document.getElementById('root')).render(
    <StrictMode>
     <BrowserRouter>
