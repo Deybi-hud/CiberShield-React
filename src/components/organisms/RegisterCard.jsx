543a2cab4634be34947b764b34f6e91311140457
Deybi-hud <hu4nt4r@gmail.com>
2025-11-21 19:01:27 -0300

    Update

--- src/components/organisms/RegisterCard.jsx
+++ src/components/organisms/RegisterCard.jsx
@@ -4,24 +4,24 @@ import Text from '../atoms/Text';
 import RegisterForm from '../molecules/RegisterForm';
 import Link from '../atoms/Link';
 
-const RegisterCard = ({ 
-  formData, 
-  onChange, 
-  errors, 
-  isLoading, 
-  onSubmit 
+const RegisterCard = ({
+  formData,
+  onChange,
+  errors,
+  isLoading,
+  onSubmit
 }) => {
   return (
     <>
-      <Image 
-        src="/assets/img/login/user.png" 
-        alt="Registro de Usuario" 
+      <Image
+        src="/assets/img/login/user.png"
+        alt="Registro de Usuario"
         className="user-image"
       />
       <Text as="h2" className="login-title">
         Crear Cuenta
       </Text>
-      
+
       <RegisterForm
         formData={formData}
         onChange={onChange}
@@ -29,7 +29,7 @@ const RegisterCard = ({
         isLoading={isLoading}
         onSubmit={onSubmit}
       />
-        
+
       <div style={{ marginTop: '1rem', textAlign: 'center' }}>
         <Link to="/login" className="login-link">
           ¿Ya tienes cuenta? Inicia Sesión
