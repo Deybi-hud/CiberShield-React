242e7455b9fa1ae26c5efaf6e50e3c366c3376cc
Deybi-hud <hu4nt4r@gmail.com>
2025-11-23 17:40:58 -0300

    Revert "Update"
    This reverts commit a17613837f9c9d4e21765fcadd51012637ca8f4f.

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './styles/Utils.css'
 import './styles/Global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ErrorBoundary>
       <BrowserRouter>
         <CarritoProvider>
           <App />
         </CarritoProvider>
       </BrowserRouter>
-    </ErrorBoundary>
   </StrictMode>
 )
