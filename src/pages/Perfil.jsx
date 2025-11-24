cdd2b04035439f66c0eed3639029f2ad1971ad58
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-24 10:30:56 -0300

    sd

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -41,6 +41,23 @@ const Perfil = () => {
     }
   };
 
+   useEffect(() => {
+      let resultado = productos;
+  
+      if (categoriaActiva !== "todos") {
+        resultado = resultado.filter((p) => {
+          if (p.categoria && p.categoria.id) {
+            return p.categoria.id === categoriaActiva;
+          }
+          if (typeof p.categoria === 'string') {
+            return p.categoria === categoriaActiva;
+          }
+          return false;
+        });
+      }
+    });
+
+
   const handleLogout = () => {
     logout();
     navigate('/login');
