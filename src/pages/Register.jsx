b9b749d62877d9ea5c964ce2ae93ec8368669c09
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-11-24 00:57:48 -0300

    asd

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RegisterCard from '../components/organisms/RegisterCard';
-import { LoginService } from '../services/Index';
+import { LoginService } from '../services/index';
 
 const Register = () => {
   const navigate = useNavigate();
