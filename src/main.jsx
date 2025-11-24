3f97a0cd0e1c6d7f7140a7dd9f874b25d72a9275
Deybi-hud <hu4nt4r@gmail.com>
2025-11-24 01:17:41 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext.jsx'
+import { AuthProvider } from './context/AuthContext';
 import './styles/Utils.css'
 import './styles/Global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <CarritoProvider>
           <App />
         </CarritoProvider>
-      </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 )
