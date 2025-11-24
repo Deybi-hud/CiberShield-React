f1f99790831248c2f130f637faed513d81dad1b8
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-11-24 00:57:33 -0300

    e

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,7 +4,7 @@ import Wrapper from "../components/templates/Wrapper";
 import SidebarHome from "../components/organisms/SidebarHome";
 import MainProductDetail from "../components/organisms/MainProductDetail";
 import { useCarrito } from "../context/CarritoContext";
-import { ProductoService } from "../services/Index";
+import { ProductoService } from "../services/index";
 
 const ProductDetail = () => {
   const { id } = useParams();
